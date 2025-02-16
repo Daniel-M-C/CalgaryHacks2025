bf984996825eebd68a761553f60f8509fb2afc76
Anooneee <72642342+Anooneee@users.noreply.github.com>
2025-02-16 08:47:14 -0700

    m

--- gamemanager.gd
+++ gamemanager.gd
@@ -1,9 +1,26 @@
 extends Node
 
-
+var player
+var not_in_water_setup = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_player()
+
+func _process(delta):
+	if player:
+		if player.in_water and not_in_water_setup:
+			var time = $WorldMusic.get_playback_position()
+			$WaterMusic.seek(time)
+			$WaterMusic.play()
+			$WorldMusic.stop()
+			not_in_water_setup = false
+		if !player.in_water and !not_in_water_setup:
+			var time = $WaterMusic.get_playback_position()
+			$WorldMusic.seek(time)
+			$WorldMusic.play()
+			$WaterMusic.stop()
+			not_in_water_setup = true
+			
 
 func _input(event):
 	if event.is_action_pressed("mouse_1"):
@@ -14,3 +31,10 @@ func _input(event):
 func _on_audio_stream_player_finished():
 	$WorldMusic.play()
 	pass # Replace with function body.
+
+func get_player():
+	if get_tree().get_first_node_in_group("player"):
+		player = get_tree().get_first_node_in_group("player")
+	else: 
+		await get_tree().process_frame
+		get_player()
