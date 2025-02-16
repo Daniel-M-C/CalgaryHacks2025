0ccab61d4c0b5b996444a473e9ace88cce7feea0
Anooneee <72642342+Anooneee@users.noreply.github.com>
2025-02-15 22:09:27 -0700

    kihihilh

--- World/AnimalInteract/plug.gd
+++ World/AnimalInteract/plug.gd
@@ -1,14 +1,17 @@
 extends StaticBody2D
+@export var activated = false
+@export var start_cable : Node
 
 func _ready():
-	pass 
-	
-func _process(delta):
-	pass
+	if start_cable:
+		connect_plug(start_cable)
 
 func connect_plug(cable):
 	print(cable)
 	$PinJoint2D.set_node_b(cable.get_path())
+	activated = true
+	
 	pass
 func disconnect_plug():
 	$PinJoint2D.set_node_b("")
+	activated = false
