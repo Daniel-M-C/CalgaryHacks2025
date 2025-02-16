853b732a6bfe8eb8e107a5b22e13689b4ede8d29
Anooneee <72642342+Anooneee@users.noreply.github.com>
2025-02-16 02:02:48 -0700

    so little, yet so much time

--- Animal/player_one.gd
+++ Animal/player_one.gd
@@ -6,9 +6,14 @@ const JUMP_VELOCITY = -400.0
 const MAX_HP = 100
 var hp = MAX_HP
 var carry_objext = ""
+var not_landed_yet = false
+var still_jumping = false
+var in_water = false
+
+@onready var sprite = $Sprite/AnimatedSprite2D
 
 func _physics_process(delta):
-	
+	print(in_water)
 	if carry_objext:
 		if global_position.x - carry_objext.global_position.x > 50 or global_position.y - carry_objext.global_position.y > 50:
 			$GrabPoint.set_node_b("")
@@ -16,29 +21,45 @@ func _physics_process(delta):
 	
 	
 	if not is_on_floor():
+		not_landed_yet = true
 		velocity += get_gravity() * delta
-		if velocity.y < 0:
-			pass # play jump up
 		if velocity.y > 0:
+			sprite.play("falling")
+			pass # play jump up
+		if velocity.y < 0:
+			if !still_jumping:
+				sprite.play("jumping")
 			pass # play jump down
+	elif not_landed_yet:
+		sprite.play("fall")
+		not_landed_yet = false
 
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$Audio/JumpSound.play()
+		sprite.play("jump")
+		still_jumping = true
 		pass # play jump start
 	
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		if velocity.y == 0:
+		if velocity.y == 0 and is_on_floor():
+			sprite.play("walk")
 			pass # play walking
 		velocity.x = direction * SPEED
 		if velocity.x < 0:
-			$Sprite/AnimatedSprite2D.scale.x = -0.25
+			sprite.scale.x = 1
+			$Tail.position.x = 7.0
 		elif velocity.x > 0:
-			$Sprite/AnimatedSprite2D.scale.x = 0.25
+			sprite.scale.x = -1
+			$Tail.position.x = -2.0
 	else:
+		if is_on_floor():
+			sprite.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if in_water:
+		velocity.x /= 1.25
+		velocity.y /= 1.25
 	move_and_slide()
 
 
@@ -82,3 +103,8 @@ func interact(action_object: Node):
 			
 		pass
 	pass
+
+
+func _on_animated_sprite_2d_animation_finished():
+	still_jumping = false
+	pass # Replace with function body.
--- World/AnimalInteract/plug.gd
+++ World/AnimalInteract/plug.gd
@@ -1,16 +1,34 @@
 extends StaticBody2D
-@export var start_cable : Node
+#@export var start_cable : Node
 @export var flow_puzzle : Node
+enum COLOUR {
+	RED,
+	GREEN,
+	YELLOW
+}
+
+@export var colour : COLOUR = COLOUR.RED:
+	set(val):
+		colour = val
 
 
 func _ready():
-	if start_cable:
-		connect_plug(start_cable)
+	##if start_cable:
+	##	connect_plug(start_cable)
+	pass
 
 func connect_plug(cable):
 	print(cable)
 	$PinJoint2D.set_node_b(cable.get_path())
+	match colour:
+		COLOUR.RED:
+			flow_puzzle.active_color = flow_puzzle.PIPE_COLOR.RED
+		COLOUR.GREEN:
+			flow_puzzle.active_color = flow_puzzle.PIPE_COLOR.GREEN
+		COLOUR.YELLOW:
+			flow_puzzle.active_color = flow_puzzle.PIPE_COLOR.YELLOW
 	
 	pass
 func disconnect_plug():
 	$PinJoint2D.set_node_b("")
+	flow_puzzle.active_color = -1
