f0395d2fd6011e64fef5dc542a3497dcc54778df
Alpaca404 <32599763+Daniel-M-C@users.noreply.github.com>
2025-02-16 04:29:37 -0700

    Turn on puzzle screen with camera

--- World/camera_2d.gd
+++ World/camera_2d.gd
@@ -6,6 +6,7 @@ const move_speed_lerp : float = 1
 
 var level_num : int = 0
 
+@export var puzzle_canvas_layer : Node 
 
 func _on_camera_trigger_body_entered(body: Node2D) -> void:
 	if is_instance_of(body, Player):#body.get_script() is Player:
@@ -14,3 +15,8 @@ func _on_camera_trigger_body_entered(body: Node2D) -> void:
 
 func _process(delta: float) -> void:
 	global_position = global_position.lerp(Vector2(level_position_offset * level_num, global_position.y), move_speed_lerp * delta)
+	
+	# turning on the screen when we get there.
+	if global_position.x > level_position_offset - 100 \
+		and puzzle_canvas_layer:
+			puzzle_canvas_layer.visible = true
